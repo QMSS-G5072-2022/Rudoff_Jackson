6a9c50b7390b1d6729eeb8ab82b04c39566adc4e
jer164 <62763243+jer164@users.noreply.github.com>
2022-10-23 16:31:42 -0400

    added HW6 stuff

---
+++ hw06/test_cipher.py
@@ -0,0 +1,31 @@
+import pytest
+from cipher import *
+
+def test_word():
+    example = 'word'
+    shift = 4
+    expected = 'Asvh'
+    actual = cipher(example, shift)
+    assert actual == expected
+
+def test_neg():
+    test_text = "The New York Mets were once good at baseball and then they sucked"
+    example_shift = -2
+    expected = "Rfc Lcu Wmpi Kcrq ucpc mlac emmb Yr ZYqcZYjj Ylb rfcl rfcw qsaicb"
+    actual = cipher(test_text, example_shift)
+    assert actual == expected
+
+
+def test_special():
+    test_text = "UhOh!?!%"
+    test_shift = 5
+    expected = "ZmTm!?!%"
+    actual = cipher(test_text, test_shift)
+    assert expected == actual
+
+
+def test_assert():
+    with pytest.raises(Exception):
+        test_word = "two"
+        test_shift = "two"
+        cipher(test_word,test_shift)
